dd0e687c9127252876736ebc7af5daafa1bcada6
azyz08 <ibroximova014@gmail.com>
2024-06-23 16:39:45 +0500

    cards

--- src/pages/azizbek/index.jsx
+++ src/pages/azizbek/index.jsx
@@ -1,29 +1,95 @@
 import "./style.scss"
 import React from 'react';
 import { Carousel } from 'antd';
-
 export default function Azizbek() {
     return (
         <>
-            <Carousel className="carousel" arrows infinite={false}>
-                <div>
-                    <div className="text">
-                        <h1>The new phones are <br /> here take a look.</h1>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Scelerisque in est <br /> dui, aliquam, tempor. Faucibus morbi turpis.</p>
-                        <a href="">Explore</a>
+            <div className="carCard">
+                <Carousel className="carousel" arrows infinite={false}>
+                    <div>
+                        <h3>
+                            <div className="text">
+                                <h1>The new phones are  here take a look.</h1>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Scelerisque in est  dui, aliquam, tempor. Faucibus morbi turpis.</p>
+                                <a href="">Explore</a>
+                            </div>
+                            <img src="https://s3-alpha-sig.figma.com/img/3c1e/4caa/09390e4696bd452c8420dbe4a7164d7b?Expires=1719792000&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=M5OFF4mlfgFkZl7yBqGqNxyB30kvlCvy5FqgXw1s-m7m9hQ0Lfh8wPRSwOzQm5PJoRgZUgQS0iq3H6W~BY7UVy96gLLKkKsZR~kzXSiIe98hNw0JA-RUaybBoJsZzCq0-49d0dyXFIxtpjenaYhiHJN5cwIgN1GFVuyfYAsHVgJ2WsVBHH7B-DQO7vBE~~BfTs0rhPytTpYBOpadlpf9CEdw8OHWpmOtyoYXGQRmP6~i9CjH~-RFPgiEwUONB54UxjbwAniuu-rZ7z0QigIYA-kZtiufhGo2mKtMGxhWsB9Lirq9ITNr6BIcFw-oEk7q1Q~5dHjyggJ1TRWY0M4PNQ__" alt="" />
+                        </h3>
                     </div>
-                <img src="https://s3-alpha-sig.figma.com/img/3c1e/4caa/09390e4696bd452c8420dbe4a7164d7b?Expires=1719792000&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=M5OFF4mlfgFkZl7yBqGqNxyB30kvlCvy5FqgXw1s-m7m9hQ0Lfh8wPRSwOzQm5PJoRgZUgQS0iq3H6W~BY7UVy96gLLKkKsZR~kzXSiIe98hNw0JA-RUaybBoJsZzCq0-49d0dyXFIxtpjenaYhiHJN5cwIgN1GFVuyfYAsHVgJ2WsVBHH7B-DQO7vBE~~BfTs0rhPytTpYBOpadlpf9CEdw8OHWpmOtyoYXGQRmP6~i9CjH~-RFPgiEwUONB54UxjbwAniuu-rZ7z0QigIYA-kZtiufhGo2mKtMGxhWsB9Lirq9ITNr6BIcFw-oEk7q1Q~5dHjyggJ1TRWY0M4PNQ__" alt="" />
-                </div>
-                <div>
+                    <div>
+                        <h3>
+                            <div className="text">
+                                <h1>The new phones are  here take a look.</h1>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Scelerisque in est  dui, aliquam, tempor. Faucibus morbi turpis.</p>
+                                <a href="">Explore</a>
+                            </div>
+                            <img src="https://s3-alpha-sig.figma.com/img/3c1e/4caa/09390e4696bd452c8420dbe4a7164d7b?Expires=1719792000&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=M5OFF4mlfgFkZl7yBqGqNxyB30kvlCvy5FqgXw1s-m7m9hQ0Lfh8wPRSwOzQm5PJoRgZUgQS0iq3H6W~BY7UVy96gLLKkKsZR~kzXSiIe98hNw0JA-RUaybBoJsZzCq0-49d0dyXFIxtpjenaYhiHJN5cwIgN1GFVuyfYAsHVgJ2WsVBHH7B-DQO7vBE~~BfTs0rhPytTpYBOpadlpf9CEdw8OHWpmOtyoYXGQRmP6~i9CjH~-RFPgiEwUONB54UxjbwAniuu-rZ7z0QigIYA-kZtiufhGo2mKtMGxhWsB9Lirq9ITNr6BIcFw-oEk7q1Q~5dHjyggJ1TRWY0M4PNQ__" alt="" />
+                        </h3>
+                    </div>
+                    <div>
+                        <h3>
+                            <div className="text">
+                                <h1>The new phones are  here take a look.</h1>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Scelerisque in est  dui, aliquam, tempor. Faucibus morbi turpis.</p>
+                                <a href="">Explore</a>
+                            </div>
+                            <img src="https://s3-alpha-sig.figma.com/img/3c1e/4caa/09390e4696bd452c8420dbe4a7164d7b?Expires=1719792000&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=M5OFF4mlfgFkZl7yBqGqNxyB30kvlCvy5FqgXw1s-m7m9hQ0Lfh8wPRSwOzQm5PJoRgZUgQS0iq3H6W~BY7UVy96gLLKkKsZR~kzXSiIe98hNw0JA-RUaybBoJsZzCq0-49d0dyXFIxtpjenaYhiHJN5cwIgN1GFVuyfYAsHVgJ2WsVBHH7B-DQO7vBE~~BfTs0rhPytTpYBOpadlpf9CEdw8OHWpmOtyoYXGQRmP6~i9CjH~-RFPgiEwUONB54UxjbwAniuu-rZ7z0QigIYA-kZtiufhGo2mKtMGxhWsB9Lirq9ITNr6BIcFw-oEk7q1Q~5dHjyggJ1TRWY0M4PNQ__" alt="" />
+                        </h3>
+                    </div>
+                    <div>
+                        <h3>
+                            <div className="text">
+                                <h1>The new phones are  here take a look.</h1>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Scelerisque in est  dui, aliquam, tempor. Faucibus morbi turpis.</p>
+                                <a href="">Explore</a>
+                            </div>
+                            <img src="https://s3-alpha-sig.figma.com/img/3c1e/4caa/09390e4696bd452c8420dbe4a7164d7b?Expires=1719792000&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=M5OFF4mlfgFkZl7yBqGqNxyB30kvlCvy5FqgXw1s-m7m9hQ0Lfh8wPRSwOzQm5PJoRgZUgQS0iq3H6W~BY7UVy96gLLKkKsZR~kzXSiIe98hNw0JA-RUaybBoJsZzCq0-49d0dyXFIxtpjenaYhiHJN5cwIgN1GFVuyfYAsHVgJ2WsVBHH7B-DQO7vBE~~BfTs0rhPytTpYBOpadlpf9CEdw8OHWpmOtyoYXGQRmP6~i9CjH~-RFPgiEwUONB54UxjbwAniuu-rZ7z0QigIYA-kZtiufhGo2mKtMGxhWsB9Lirq9ITNr6BIcFw-oEk7q1Q~5dHjyggJ1TRWY0M4PNQ__" alt="" />
+                        </h3>
+                    </div>
+                </Carousel>
+            </div>
 
-                </div>
-                <div>
+            <div className="tex">
+                <h1>Shop our latest offers and categories</h1>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Scelerisque in est dui, aliquam, tempor. Faucibus morbi turpis. Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+            </div>
 
+            <div className="bigBox">
+                <div className="cardBox">
+                    <div className="left">
+                        <div className="box box1">
+                            <img src="https://s3-alpha-sig.figma.com/img/609c/d336/10f5bce3a3242d7165d4c622572a7f34?Expires=1719792000&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=oJyMaKCWJLHKqLJ9hzNSu-xh7ahbNUaIPNvOF4It2nbCfbGHM2lv7sdQs8VWsbmkWGRA5mZRntCsl9tx4hB2xzM-HjINtoCbkEuGXwIjWWCwNYO1kJzTosm9gFNERMz04pd9xbtU2EkxglGXGG0cgHbS3VSNQ2cNeQn7sFMyAMNB05mC-wJaQnNmMqoXhv6dxe0JdRmyMO9710hr1z-MRAPlRKzCpNvJzkH31hRDRVmzKN5XfGGLhc5tQ99yVcHlcFPdcBTgCmCCMsfqwr7vxxbNufFgqTO1SQmD-pnq2Load4pJH2fabk~TXwMQjPpd3f66wehwBBzi-DBHPLj6qw__" alt="" />
+                            <div className="text">
+                                <p>Laptops</p>
+                                <h3>True Laptop <br /> Solution</h3>
+                            </div>
+                        </div>
+                        <div className="box box2">
+                            <img src="https://s3-alpha-sig.figma.com/img/382a/966d/9393f6cbe099e93a46809a2330d2eebf?Expires=1719792000&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=ImETloNf4~8xvOWWLidQsUQE2fsYjAKoz~CIN1iqTZ4WELS0u5ai4h3cUg40lJ3cg2Hyqfly5hzJOlI8kHbtjKBTQZ1H~DTkNGW7-dIjC66pLNksB04j-PAPWGKfl3a5BqPib1~yBlwS4KLkgAclxeppeavRVaeGIdA2twhxkR4P9s~SUO3i8w-hUfzO00dMJ1nKS7nyGwWxWNHHLSFTADLQWimsYaJl1-DXq2XXdIqNrZh4btdzKBaCCkXyO8m29tgrRdq5-r~ZIy-PfOiEzFSuqkQkulZEkAh7wDIJHkakgEkz93hmktxTh3BtN60dPhwOjz406Lf2bNfFdeefUg__" alt="" />
+                            <div className="text">
+                                <p>Watch</p>
+                                <h3>Not just <br /> stylisht</h3>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="right">
+                        <div className="box box3 box5">
+                            <img src="https://s3-alpha-sig.figma.com/img/40fa/449b/d25d54bfdac12913f24fb4638ff57107?Expires=1719792000&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=iSZxU4NgCIzuFuqIHUozedO2c2kM6VNIdn5LBTvscho17~mchtKJBC2Sp2HnT~LE20L3X8umVS2LSpHq6YpyOtT-cfnuIoFLdBdpIqTIW94FuMUMIDb4Gnw7EYLgmEzgWaA~pvg~BO6ao~1EpF0PtMNshV-Lick3FYPGRy6KwyUOpfxdDYuOFu3ccwZ-~cuz8DP1FgDxo2fWBKml0jyXQkHf~nOuyj2YmtuFlBDWnVYuSs2aOJzzWkJZuWkJlX-m3AA2clbBkg~mC0BRl-EDmQWd5WA-z1ETF8s3BT~dA3hCT2TtYPjE9RX5qHdk6ZSqdzy2a5HObnoWrwBFYij6QQ__" alt="" />
+                            <div className="text">
+                                <p>Phones</p>
+                                <h3>Your day to day <br /> life</h3>
+                            </div>
+                        </div>
+                        <div className="box box4 box5">
+                            <div className="text">
+                                <p>Tablet</p>
+                                <h3>Empower your <br /> work</h3>
+                            </div>
+                            <img src="https://s3-alpha-sig.figma.com/img/d179/b03d/8d6f6accf6ec8a5bbc7487f406513dea?Expires=1719792000&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=J7UPSOKz2nkz8DfK-ba8jWfpisu6meq6xofRKSaBtLBx8ecfpmnO5t4E0xv9WDESUQUMJifTOg5-JMLv-oDdM8FZ2ytPjIam0Cq5cJfPwWOijVYz-LIVdSxqiOamvjl8jg7J64dWisiJ7haV~aY9mdoecAAz1H9-dUQKps0livYnXvR9zLTwzt~CAPbqco2yqCm-SqWYPxdFgLPZxtrv4ao2YxyVN0XSN-ejYgf27KJmpes~rF0tds4i-ZZcXt6Di785ct5G1vWaAJNGNT-pbhpcWmG0Wv-GU0zruIcE4xRg~t2sID-3yPD3xPdotUAHOU5AoKFq2sIvT3TufUtlBg__" alt="" />
+                        </div>
+                    </div>
                 </div>
-                <div>
-
-                </div>
-            </Carousel>
+            </div>
         </>
     )
 }
