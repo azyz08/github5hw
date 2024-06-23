4e054dcd370abd2a364acc17b1c445a62bb44c37
azyz08 <ibroximova014@gmail.com>
2024-06-23 14:46:30 +0500

    navbar

--- src/components/navbar/index.jsx
+++ src/components/navbar/index.jsx
@@ -3,13 +3,56 @@ import "./style.scss"
 import Azizbek from "../../pages/azizbek"
 import Abduxabir from "../../pages/abduxabir"
 
+import React from 'react';
+import { DownOutlined } from '@ant-design/icons';
+import { Dropdown, Space } from 'antd';
+const items = [
+    {
+        label: <Link to={"/"}>Azizbek</Link>,
+        key: '0',
+    },
+    {
+        label: <Link to={"/page2"}>Abduxabir</Link>,
+        key: '1',
+    },
+    {
+        type: 'divider',
+    },
+];
+
 export default function Navbar() {
     return (
         <>
             <div className="nav">
-                <Link to={"/"}>Azizbek</Link>
-                <Link to={"/page2"}>Abduxabir</Link>
+                <Link to={"/"}><h2>CALIFORNIA</h2></Link>
+                <input type="checkbox" id="m" />
+                <ul>
+                    <div className="labelBox">
+                        <label htmlFor="m"><i class="fa-solid fa-xmark"></i></label>
+                        <a href=""><i class="fa-solid fa-cart-shopping"></i></a>
+                    </div>
+                    <Link to={"/"}><h3>Azizbek</h3></Link>
+                    <Link to={"/page2"}><h3>Abduxabir</h3></Link>
+                    <Link to={"/page2"}><h3>Support</h3></Link>
+                    <h3>
+                        <Dropdown menu={{ items, }} trigger={['click']}>
+                            <a onClick={(e) => e.preventDefault()}>
+                                <Space className="solu">
+                                    Solutions
+                                    <DownOutlined />
+                                </Space>
+                            </a>
+                        </Dropdown>
+                    </h3>
+                </ul>
+                <div className="icon">
+                    <i class="fa-solid fa-magnifying-glass"></i>
+                    <a href=""><i class="fa-solid fa-cart-shopping"></i></a>
+                    <label htmlFor="m"><i class="fa-solid fa-bars"></i></label>
+                </div>
             </div>
+
+            <p className="nb">.</p>
 
             <Routes>
                 <Route path="/" element={<Azizbek />} />
